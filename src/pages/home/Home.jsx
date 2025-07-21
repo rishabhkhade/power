6213a8f38e1ab4501a6433a91d08c00d99e27cae
Rishabh <rishabhkhade95@gmail.com>
2025-07-22 00:20:42 +0530

    pp

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -80,7 +80,7 @@ const data = [
               spaceBetween={30}
               centeredSlides={true}
               autoplay={{
-                delay: 2500,
+                delay: 250000,
                 disableOnInteraction: false,
               }}
               pagination={{
